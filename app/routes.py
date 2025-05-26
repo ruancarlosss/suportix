7388db9c3944e2d6f4826fd18ff72e4fdc14e659
Ruan C. De Souza <ruancarlosdesousa370@gmail.com>
2025-05-26 20:10:03 -0300

    Somente usuários logados podem abrir chamados. E o chamado será atribuido ao id do usuário em questão

--- app/routes.py
+++ app/routes.py
@@ -1,4 +1,5 @@
 from flask import Blueprint, render_template, request, redirect, url_for
+from flask_login import login_required, current_user
 from app.models import Ticket
 from app import db
 
@@ -12,11 +13,19 @@ def  index():
 
 
 @main.route('/create', methods=['GET', 'POST'])
+
 def create_ticket():
+@login_required
+
     if request.method == 'POST':
         title = request.form['title']
         desc = request.form['description']
         new = Ticket(title=title, description=desc)
+        new = Ticket(
+            title=title,
+            description=desc,
+            user_id=current_user.id # Vincula o Ticket ao usuário logado
+        )
         db.session.add(new)
         db.session.commit()
         return redirect(url_for('main.index'))
